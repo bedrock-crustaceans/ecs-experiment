0fd1421732319bfbed9dcab63831c9d0480dbd26
= <44266876+RadiatedMonkey@users.noreply.github.com>
2024-08-10 22:28:49 +0200

    Implement QueryParams::fetch for singles and tuples

--- src/query.rs
+++ src/query.rs
@@ -4,23 +4,23 @@ use std::{marker::PhantomData, sync::Arc};
 use std::mem::MaybeUninit;
 
 use crate::component::{Component, Components, TypedStorage};
-use crate::{Entity, EntityIter, sealed, World};
+use crate::{sealed, Entity, EntityId, EntityIter, Filter, World};
 use crate::filter::FilterParams;
 use crate::sealed::Sealed;
 
-pub trait NonRefQueryParam {
+pub trait DerefQueryParam {
     type Ref<'q>;
 }
 
-impl NonRefQueryParam for Entity {
+impl DerefQueryParam for Entity {
     type Ref<'q> = Self;
 }
 
-impl<T: Component> NonRefQueryParam for T {
+impl<T: Component> DerefQueryParam for T {
     type Ref<'q> = &'q T;
 }
 
-impl<T1: Component, T2: Component> NonRefQueryParam for (T1, T2) {
+impl<T1: Component, T2: Component> DerefQueryParam for (T1, T2) {
     type Ref<'q> = (&'q T1, &'q T2);
 }
 
@@ -48,6 +48,14 @@ pub trait QueryParams: Sized {
     /// Calling this function in any other situation will lead to undefined behaviour.
     #[doc(hidden)]
     unsafe fn unlock_all<S: sealed::Sealed>(components: &Components);
+    /// Fetches the requested components from a world.
+    /// 
+    /// The `F` generic parameter is there to allow passing filters to the fetch method.
+    /// 
+    /// # Returns
+    /// 
+    /// This function returns a tuple of the fetched component(s) and the amount the iterator index should be advanced by.
+    fn fetch<F: FilterParams>(world: &Arc<World>, index: usize, lock: &LockFlag) -> (Option<Self>, usize);
 }
 
 impl QueryParams for Entity {
@@ -58,6 +66,10 @@ impl QueryParams for Entity {
 
     // Requesting entities uses no locking, so this function is empty.
     unsafe fn unlock_all<S: Sealed>(_components: &Components) {}
+
+    fn fetch<F: FilterParams>(world: &Arc<World>, index: usize, lock: &LockFlag) -> (Option<Self>, usize) {
+        todo!()
+    }
 }
 
 impl<T: Component + 'static> QueryParams for &T {
@@ -81,6 +93,89 @@ impl<T: Component + 'static> QueryParams for &T {
             typed_store.storage.force_unlock_read();
         }
     }
+
+    fn fetch<F: FilterParams>(world: &Arc<World>, mut index: usize, lock: &LockFlag) -> (Option<Self>, usize) {
+        let start_index = index;
+
+        let type_id = TypeId::of::<Self::NonRef>();
+        let typeless_store = world.components.map.get(&type_id);
+            if let Some(store) = typeless_store {
+                let typed_store = store
+                    .value()
+                    .as_any()
+                    .downcast_ref::<TypedStorage<T>>()
+                    .unwrap();
+
+                // Lock the store while QueryIter exists.
+                let store_lock = if index == 0 {
+                    // Acquire lock
+                    lock.flag();
+                    typed_store.storage.read()
+                } else {
+                    // Retrieve lock
+                    // SAFETY: This is safe because this query has acquired the lock on the first iteration.
+                    // Additionally, this lock has been forgotten and therefore this thread logically still owns the lock.
+                    // In case a second iterator is created from the same query, the lock flag will still be set.
+                    unsafe { typed_store.storage.make_read_guard_unchecked() }
+                };
+
+                // Find next entity ID. This also filters out the entities that do not match the filter.
+                let store_index = loop {
+                    let Some(store_index) = typed_store.reverse_map.read().get(index).map(|id| *id) else {
+                        // No more components remaining.
+                        std::mem::forget(store_lock);
+                        return (None, index - start_index);
+                    };
+
+                    let entity = Entity {
+                        id: store_index,
+                        world: world.clone()
+                    };
+
+                    if F::filter(&entity) {
+                        // Matching entity has been found.
+                        break store_index
+                    }
+
+                    // Continue to next option
+                    index += 1;
+                };
+
+                // ZSTs need different treatment
+                let fetched = if std::mem::size_of::<T>() == 0 {
+                    // SAFETY: This is safe because a ZST does not need initialisation.
+                    // It is also impossible to construct unconstructable types such as empty
+                    // enums since the types have to be constructed to add them to an entity
+                    // in the first place.
+                    Some(unsafe {
+                        MaybeUninit::uninit().assume_init()
+                    })
+                } else {
+                    // println!("{}", std::any::type_name::<P>());
+                    // println!("{}", std::any::type_name::<Self::Item>());
+
+                    // SAFETY: This is simply to get around issues with the type system.
+                    // An assertion at the start of the iterator ensures that both types below are
+                    // equal to each other. The transmuted component will also not have a longer
+                    // lifetime than the original because its lifetime will be bounded to the query.
+                    // The container of the component lives longer than the query and it can also not
+                    // be modified while this query exists.
+                    Some(unsafe {
+                        // std::mem::transmute_copy::<&P, Self::Item>(&&store_lock[self.index])
+                        std::mem::transmute_copy::<&T, _>(&&store_lock[store_index.0])
+                        // &store_lock[store_index.0]
+                    })
+                };
+
+                std::mem::forget(store_lock);
+                index += 1;
+                
+                (fetched, index - start_index)
+            } else {
+                // This component is not owned by any entity
+                (None, index - start_index)
+            }
+    }
 }
 
 impl<T: Component> QueryParams for &mut T {
@@ -89,6 +184,10 @@ impl<T: Component> QueryParams for &mut T {
     const SHARED: bool = false;
 
     unsafe fn unlock_all<S: sealed::Sealed>(components: &Components) {
+        todo!()
+    }
+
+    fn fetch<F: FilterParams>(world: &Arc<World>, index: usize, lock: &LockFlag) -> (Option<Self>, usize) {
         todo!()
     }
 }
@@ -104,6 +203,26 @@ where
 
     unsafe fn unlock_all<S: sealed::Sealed>(components: &Components) {
         todo!()
+    }
+
+    fn fetch<F: FilterParams>(world: &Arc<World>, mut index: usize, lock: &LockFlag) -> (Option<Self>, usize) {
+        let start = index;
+
+        let (t1, adv) = T1::fetch::<F>(world, index, lock);
+        index += adv;
+
+        let Some(t1) = t1 else {
+            return (None, index - start)
+        };
+
+        let (t2, adv) = T2::fetch::<F>(world, index, lock);
+        index += adv;
+
+        let Some(t2) = t2 else {
+            return (None, index - start)
+        };
+
+        (Some((t1, t2)), index - start)
     }
 }
 
@@ -182,11 +301,12 @@ impl<Q: QueryParams, F: FilterParams> Query<Q, F> {
 
 impl<'query, P, F, N> IntoIterator for &'query Query<N, F>
 where
-    P: NonRefQueryParam + 'static,
+    P: DerefQueryParam + 'static,
     F: FilterParams,
     N: QueryParams<NonRef = P>,
 {
-    type Item = P::Ref<'query>;
+    // type Item = P::Ref<'query>;
+    type Item = N;
     type IntoIter = QueryIter<'query, P, N, F>;
 
     fn into_iter(self) -> Self::IntoIter {
@@ -230,11 +350,12 @@ where
 
 impl<'query, P, F, Q> Iterator for QueryIter<'query, P, Q, F>
 where
-    P: NonRefQueryParam + 'static,
+    P: DerefQueryParam + 'static,
     F: FilterParams,
     Q: QueryParams<NonRef = P>
 {
-    type Item = P::Ref<'query>;
+    // type Item = P::Ref<'query>;
+    type Item = Q;
 
     fn next(&mut self) -> Option<Self::Item> {
         if Q::IS_ENTITY {
@@ -253,89 +374,94 @@ where
 
             // todo!("filters");
 
-            let typeless_store = self.world.components.map.get(&TypeId::of::<Q::NonRef>());
+            let (components, advance) = Q::fetch::<F>(&self.world, self.index, &self.locked);
+            self.index += advance;
 
-            if let Some(store) = typeless_store {
-                let typed_store = store
-                    .value()
-                    .as_any()
-                    .downcast_ref::<TypedStorage<P>>()
-                    .unwrap();
+            components
 
-                // Lock the store while QueryIter exists.
-                let store_lock = if self.index == 0 {
-                    // Acquire lock
-                    self.locked.flag();
-                    typed_store.storage.read()
-                } else {
-                    // Retrieve lock
-                    // SAFETY: This is safe because this query has acquired the lock on the first iteration.
-                    // Additionally, this lock has been forgotten and therefore this thread logically still owns the lock.
-                    // In case a second iterator is created from the same query, the lock flag will still be set.
-                    unsafe { typed_store.storage.make_read_guard_unchecked() }
-                };
+            // let typeless_store = self.world.components.map.get(&TypeId::of::<Q::NonRef>());
 
-                // Find next entity ID. This also filters out the entities that do not match the filter.
-                let store_index = loop {
-                    let Some(store_index) = typed_store.reverse_map.read().get(self.index).map(|id| *id) else {
-                        // No more components remaining.
-                        std::mem::forget(store_lock);
-                        return None;
-                    };
+            // if let Some(store) = typeless_store {
+            //     let typed_store = store
+            //         .value()
+            //         .as_any()
+            //         .downcast_ref::<TypedStorage<P>>()
+            //         .unwrap();
 
-                    let entity = Entity {
-                        id: store_index,
-                        world: self.world.clone()
-                    };
+            //     // Lock the store while QueryIter exists.
+            //     let store_lock = if self.index == 0 {
+            //         // Acquire lock
+            //         self.locked.flag();
+            //         typed_store.storage.read()
+            //     } else {
+            //         // Retrieve lock
+            //         // SAFETY: This is safe because this query has acquired the lock on the first iteration.
+            //         // Additionally, this lock has been forgotten and therefore this thread logically still owns the lock.
+            //         // In case a second iterator is created from the same query, the lock flag will still be set.
+            //         unsafe { typed_store.storage.make_read_guard_unchecked() }
+            //     };
 
-                    if F::filter(&entity) {
-                        // Matching entity has been found.
-                        break store_index
-                    }
+            //     // Find next entity ID. This also filters out the entities that do not match the filter.
+            //     let store_index = loop {
+            //         let Some(store_index) = typed_store.reverse_map.read().get(self.index).map(|id| *id) else {
+            //             // No more components remaining.
+            //             std::mem::forget(store_lock);
+            //             return None;
+            //         };
 
-                    // Continue to next option
-                    self.index += 1;
-                };
+            //         let entity = Entity {
+            //             id: store_index,
+            //             world: self.world.clone()
+            //         };
 
-                let item = match Q::SHARED {
-                    true => {
-                        // ZSTs need different treatment
-                        if std::mem::size_of::<P>() == 0 {
-                            // SAFETY: This is safe because a ZST does not need initialisation.
-                            // It is also impossible to construct unconstructable types such as empty
-                            // enums since the types have to be constructed to add them to an entity
-                            // in the first place.
-                            Some(unsafe {
-                                MaybeUninit::uninit().assume_init()
-                            })
-                        } else {
-                            // println!("{}", std::any::type_name::<P>());
-                            // println!("{}", std::any::type_name::<Self::Item>());
+            //         if F::filter(&entity) {
+            //             // Matching entity has been found.
+            //             break store_index
+            //         }
 
-                            // SAFETY: This is simply to get around issues with the type system.
-                            // An assertion at the start of the iterator ensures that both types below are
-                            // equal to each other. The transmuted component will also not have a longer
-                            // lifetime than the original because its lifetime will be bounded to the query.
-                            // The container of the component lives longer than the query and it can also not
-                            // be modified while this query exists.
-                            Some(unsafe {
-                                // std::mem::transmute_copy::<&P, Self::Item>(&&store_lock[self.index])
-                                std::mem::transmute_copy::<&P, Self::Item>(&&store_lock[store_index.0])
-                            })
-                        }
-                    }
-                    false => {
-                        todo!("Single mutable fetch")
-                    }
-                };
+            //         // Continue to next option
+            //         self.index += 1;
+            //     };
 
-                std::mem::forget(store_lock);
-                self.index += 1;
-                item
-            } else {
-                // This component is not owned by any entity
-                None
-            }
+            //     let item = match Q::SHARED {
+            //         true => {
+            //             // ZSTs need different treatment
+            //             if std::mem::size_of::<P>() == 0 {
+            //                 // SAFETY: This is safe because a ZST does not need initialisation.
+            //                 // It is also impossible to construct unconstructable types such as empty
+            //                 // enums since the types have to be constructed to add them to an entity
+            //                 // in the first place.
+            //                 Some(unsafe {
+            //                     MaybeUninit::uninit().assume_init()
+            //                 })
+            //             } else {
+            //                 // println!("{}", std::any::type_name::<P>());
+            //                 // println!("{}", std::any::type_name::<Self::Item>());
+
+            //                 // SAFETY: This is simply to get around issues with the type system.
+            //                 // An assertion at the start of the iterator ensures that both types below are
+            //                 // equal to each other. The transmuted component will also not have a longer
+            //                 // lifetime than the original because its lifetime will be bounded to the query.
+            //                 // The container of the component lives longer than the query and it can also not
+            //                 // be modified while this query exists.
+            //                 Some(unsafe {
+            //                     // std::mem::transmute_copy::<&P, Self::Item>(&&store_lock[self.index])
+            //                     std::mem::transmute_copy::<&P, Self::Item>(&&store_lock[store_index.0])
+            //                 })
+            //             }
+            //         }
+            //         false => {
+            //             todo!("Single mutable fetch")
+            //         }
+            //     };
+
+            //     std::mem::forget(store_lock);
+            //     self.index += 1;
+            //     item
+            // } else {
+            //     // This component is not owned by any entity
+            //     None
+            // }
         }
     }
 }
